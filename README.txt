89cc06c64d2d70bd9a33cebb388dccb6c021c49b
vasudha shakya <eeevasudha@yahoo.com>
2013-09-17 17:11:17 -0700

    git commit on local

---
+++ README.txt
@@ -0,0 +1,24 @@
+README
+Flower Gallery Website
+
+Zip file contains
+Rails MVC structure 
+	Controllers(2)
+	Models(2)
+	Views
+	Test(rake test result)
+
+Working
+on a Rails Server
+http://localhost:3000/flowers
+
+Functionality
+Display completed as in Mockup
+Flower gallery display on front page
+CRUD Rest API demonstration
+Clicking on image leads to new model
+Pagination implemented
+
+Images:
+The picture images are from my family photostream 
+"http://www.flickr.com/photos/subodhs/"	used with permission
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Sandbox::Application.config.secret_key_base = '404cacecb4dd34e9aba0a962de5910a55a6d627de844c0dee766cae5a7db8c06e3f681907da2e8b945aed0b1ae5c046ccfb1505cbcda2b8a837d98b447e1749a'
